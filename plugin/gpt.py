4f3a2e83c05811a2e5cbea6d4902f78c36b51799
retr0reg <patrick_0429@163.com>
2023-12-31 22:10:48 +0800

    finished gdb part

---
+++ plugin/gpt.py
@@ -0,0 +1,87 @@
+import gdb
+import requests
+import time
+
+import gdb
+import os
+import signal
+
+def interrupt_program():
+    """Sends a SIGINT to the debugged process to simulate Ctrl+C."""
+    # 获取当前选中的进程
+    inferior = gdb.selected_inferior()
+
+    if inferior is not None and inferior.pid > 0:
+        # 发送SIGINT信号到进程
+        os.kill(inferior.pid, signal.SIGINT)
+        print(f"Sent SIGINT to process with PID: {inferior.pid}")
+    else:
+        print("No process is currently being debugged.")
+
+# 在GDB Python API中封装成命令
+class InterruptCommand(gdb.Command):
+    """Interrupt the execution of the debugged program (like Ctrl+C)"""
+
+    def __init__(self):
+        super(InterruptCommand, self).__init__("interrupt", gdb.COMMAND_USER)
+
+    def invoke(self, arg, from_tty):
+        interrupt_program()
+
+InterruptCommand()
+
+import base64
+
+def encode_response(text):
+    text_bytes = text.encode('utf-8')
+    base64_bytes = base64.b64encode(text_bytes)
+    return base64_bytes.decode('utf-8')
+
+SERVER = "192.168.31.251:5000"
+
+def send_response(response, command):
+    try:
+        # Adjust the field names if necessary to match the expected schema
+        json_payload = {"response": encode_response(response), "instruction": command}
+        rs = requests.post(f"http://{SERVER}/see-callback/", json=json_payload)
+        # print(json_payload)
+        if rs.status_code == 200:
+            print("Response sent successfully.")
+        else:
+            print(f'Send_response failed, check for server connection please: {rs.status_code}')
+    except Exception as e:
+        print(f'Exception occurred while sending response: {e}')
+
+
+class ReverseShellCommand(gdb.Command):
+    "Fetch and execute commands from a remote server"
+
+    def __init__(self):
+        super(ReverseShellCommand, self).__init__("gdbgpt", gdb.COMMAND_USER)
+        self.set_asynchronous(True)
+
+    def invoke(self, arg, from_tty):
+        while True:
+            try:
+                response = requests.get(f"http://{SERVER}/get-instruction", timeout=5)
+                time.sleep(2)
+                if response.status_code == 200:
+                    data = response.json()
+                    instruction = data.get('instruction')
+
+                    if instruction:
+                        print(f"Executing instruction from server: {instruction}")
+                        responses = gdb.execute(instruction,to_string=True)
+                        send_response(response=responses,command=instruction)
+                else:
+                    if response.status_code == 404:
+                        pass
+                    else:
+                        print(f"Failed to connect to server. Status Code: {response.status_code}")
+
+            except Exception as e:
+                print(f"An error occurred: {e}")
+                time.sleep(2)
+
+InterruptCommand()
+ReverseShellCommand()
---
+++ server/main.py
@@ -0,0 +1,76 @@
+from fastapi import FastAPI, Form, Request, HTTPException
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import JSONResponse
+
+import httpx
+import base64
+
+def decode_response(encoded_text):
+    base64_bytes = encoded_text.encode('utf-8')
+    text_bytes = base64.b64decode(base64_bytes)
+    return text_bytes.decode('utf-8')
+
+SERVER = "http://192.168.31.251:5000"
+instructions_list = []
+
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")  # Make sure 'templates' directory exists
+
+# Simulated database of instructions
+instructions_queue = []
+
+
+@app.get("/add-instruction/")
+async def add_instruction(instruction: str):
+    instructions_queue.append(instruction)
+    return JSONResponse(content={"message": "Instruction added successfully"}, status_code=200)
+
+@app.get("/get-instruction")
+async def get_instruction():
+    if instructions_queue:
+        # Pop the first instruction from the queue and return it
+        instruction = instructions_queue.pop(0)
+        return {"instruction": instruction}
+    else:
+        # If the queue is empty, return an empty instruction
+        raise HTTPException(status_code=404, detail="No instruction available.")
+    
+@app.get("/", response_class=HTMLResponse)
+async def read_form(request: Request):
+    return templates.TemplateResponse("form.html", {"request": request})
+
+@app.post("/see-callback/")
+async def see_callback(request: Request):
+    item = await request.json()
+    results_dict[item['instruction']] = decode_response(item['response'])
+    return {"message": "Response received successfully"}
+
+import httpx
+import asyncio
+
+results_dict = {}
+
+async def add_instruction(instruction: str):
+    async with httpx.AsyncClient() as client:
+        response = await client.get(f"{SERVER}/add-instruction/?instruction={instruction}")
+        if response.status_code == 200:
+            print(f"Instruction '{instruction}' added successfully.")
+        else:
+            raise Exception(f"Failed to add instruction: {response.text}")
+
+async def await_callback(instruction: str):
+    while instruction not in results_dict:
+        await asyncio.sleep(1)
+    return results_dict.pop(instruction)
+
+@app.post("/instruct/")
+async def instruct(instruction: str):
+    await add_instruction(instruction)
+    response = await await_callback(instruction)
+    print(f"Received response for instruction '{instruction}': {response}")
+    return response
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=5000)
